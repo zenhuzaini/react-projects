5b295c92c17e47aaef0a606ebeb9342a34eadd70
zenex <zen.huzaini@gds.ey.com>
2025-11-01 20:38:38 +0100

    add some components

--- 2025/heyzenexblog-next/app/page.tsx
+++ 2025/heyzenexblog-next/app/page.tsx
@@ -1,3 +1,38 @@
+import Moon from "./icons/Moon";
+import Sun from "./icons/Sun";
+
 export default function Home() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<div className="">
+			<div className="grid grid-cols-6 gap-4 h-28 bg-amber-200">
+				<div className="">
+					<div>
+						<h1>
+							ZEN
+							<br />
+							HUZAINI
+						</h1>
+					</div>
+				</div>
+				<div className="col-span-2 col-start-3 col-end-4">
+					<div>
+						<nav>
+							<a>Home</a>
+							<a>Photography</a>
+							<a>Adventures</a>
+							<a>Contact</a>
+						</nav>
+					</div>
+				</div>
+
+				<div className="col-start-6 col-end-7 ">
+					<div className="flex">
+						<Sun></Sun>
+
+						<Moon></Moon>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
