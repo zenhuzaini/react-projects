c4f72c0fd6e5fdcc46db1f49412921da6e647187
zenex <zen.huzaini@gds.ey.com>
2025-11-02 22:26:46 +0100

    fix simple changes

--- 2025/heyzenexblog-next/components/molecule/Cards/PostCardsLong.tsx
+++ 2025/heyzenexblog-next/components/molecule/Cards/PostCardsLong.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight } from "lucide-react";
+import ArrowRight from "@/icons/ArrowRight";
 import { ImageWithSkeleton } from "../ImageWithSkeleton";
 import { PhotoCardDetailType } from "@/types/components";
 
