1d90e45ef9befd0d429f085a6007a3bf3e9e6b7c
zenex <zen.huzaini@gds.ey.com>
2025-11-09 13:44:16 +0100

    add story content and json

--- 2025/heyzenexblog-next/app/playground/content/page.tsx
+++ 2025/heyzenexblog-next/app/playground/content/page.tsx
@@ -17,6 +17,14 @@ import { CheckIcon, InfoIcon } from "lucide-react";
 import React from "react";
 import Map from "@/icons/Map";
 import AtSymbol from "@/icons/AtSymbol";
+import dynamic from "next/dynamic";
+
+const ReactJsonView = dynamic(() => import("@microlink/react-json-view"), {
+	ssr: false, // disables SSR for this component!
+});
+
+import { storyContentExample } from "@/mock/storyContent";
+import { DataContentType } from "@/types/data";
 
 const ContentPlayground = () => {
 	const [openStartDate, setOpenStartDate] = React.useState(false);
@@ -37,170 +45,192 @@ const ContentPlayground = () => {
 	const [komoot, setKomoot] = React.useState<string>();
 	const [otherUrl, setOtherUrl] = React.useState<string>();
 	const [photoCollections, setPhotoCollections] = React.useState<string[]>();
-	const [content, setContent] = React.useState<string[]>();
+	const [content, setContent] =
+		React.useState<DataContentType[]>(storyContentExample);
 
 	return (
-		<div className="flex flex-col gap-5 lg:gap-9">
-			<SectionTitle
-				left={
-					<>
-						<span className="font-extrabold text-primaryaccent">create</span>
-						<br />
-						<span className="font-extralight">imaginary</span>
-					</>
-				}
-				right={<>Story!</>}></SectionTitle>
-			<div className="grid lg:grid-cols-3 gap-4 h-full">
-				<div className="lg:col-span-2 w-full h-full bg-accentbackground rounded-2xl">
-					04
-				</div>
-				<div className="w-full h-full rounded-2xl">
-					<div className="grid w-full max-w-sm gap-6">
-						<InputGroup>
-							<InputGroupInput placeholder="Title" />
-							<InputGroupAddon>
-								<Chat size={4}></Chat>
-							</InputGroupAddon>
-							<InputGroupAddon align="inline-end">
-								<XMark size={4}></XMark>
-								<InfoIcon />
-							</InputGroupAddon>
-						</InputGroup>
-
-						<InputGroup>
-							<InputGroupTextarea placeholder="Ask, Search or Chat..." />
-							<InputGroupAddon align="block-end">
-								<InputGroupText className="ml-auto">
-									52 char left
-								</InputGroupText>
-								<Separator orientation="vertical" className="h-4!" />
-								<CheckIcon />
-								<InfoIcon />
-							</InputGroupAddon>
-						</InputGroup>
-
-						<div className="grid grid-cols-2 gap-3">
+		<div className="flex flex-col gap-5 lg:gap-10">
+			<div className="flex flex-col gap-5 lg:gap-9">
+				<SectionTitle
+					left={
+						<>
+							<span className="font-extrabold text-primaryaccent">create</span>
+							<br />
+							<span className="font-extralight ">your</span>
+						</>
+					}
+					right={<>Story!</>}></SectionTitle>
+				<div className="grid lg:grid-cols-3 gap-4 h-full">
+					<div className="lg:col-span-2 w-full h-full overflow-hidden ">
+						<ReactJsonView
+							src={content}
+							onAdd={(data) => {
+								setContent(data.updated_src as DataContentType[]);
+							}}
+							onDelete={(data) => {
+								setContent(data.updated_src as DataContentType[]);
+							}}
+							onEdit={(data) => {
+								setContent(data.updated_src as DataContentType[]);
+							}}
+							collapsed={true}
+							iconStyle="circle"
+						/>
+					</div>
+					<div className="w-full h-full rounded-2xl">
+						<div className="grid w-full max-w-sm gap-6">
 							<InputGroup>
-								<DatePicker
-									date={startDate}
-									setDate={(date) => setStartDate(date)}
-									open={openStartDate}
-									setOpen={() => setOpenStartDate((prev) => !prev)}
-									label="Date Start"></DatePicker>
-							</InputGroup>
-
-							<InputGroup>
-								<DatePicker
-									date={endDate}
-									setDate={(date) => setEndDate(date)}
-									open={openEndDate}
-									setOpen={() => setOpenEndDate((prev) => !prev)}
-									label="Date End"></DatePicker>
-							</InputGroup>
-						</div>
-
-						<InputGroup>
-							<InputGroupInput placeholder="photo cover Url" />
-							<InputGroupAddon>
-								<PhotoImageIcon size={4}></PhotoImageIcon>
-							</InputGroupAddon>
-							<InputGroupAddon align="inline-end">
-								<CheckIcon />
-								<InfoIcon />
-							</InputGroupAddon>
-						</InputGroup>
-						<InputGroup>
-							<InputGroupInput placeholder="Location" />
-							<InputGroupAddon>
-								<Globe size={4} />
-							</InputGroupAddon>
-							<InputGroupAddon align="inline-end">
-								<CheckIcon />
-								<InfoIcon />
-							</InputGroupAddon>
-						</InputGroup>
-						<div className="grid grid-cols-2 gap-3">
-							<InputGroup>
-								<InputGroupInput placeholder="Latitude" />
+								<InputGroupInput placeholder="Title" />
 								<InputGroupAddon>
-									<Map size={4} />
+									<Chat size={4}></Chat>
+								</InputGroupAddon>
+								<InputGroupAddon align="inline-end">
+									<XMark size={4}></XMark>
+									<InfoIcon />
+								</InputGroupAddon>
+							</InputGroup>
+
+							<InputGroup>
+								<InputGroupTextarea placeholder="Ask, Search or Chat..." />
+								<InputGroupAddon align="block-end">
+									<InputGroupText className="ml-auto">
+										52 char left
+									</InputGroupText>
+									<Separator orientation="vertical" className="h-4!" />
+									<CheckIcon />
+									<InfoIcon />
+								</InputGroupAddon>
+							</InputGroup>
+
+							<div className="grid grid-cols-2 gap-3">
+								<InputGroup>
+									<DatePicker
+										date={startDate}
+										setDate={(date) => setStartDate(date)}
+										open={openStartDate}
+										setOpen={() => setOpenStartDate((prev) => !prev)}
+										label="Date Start"></DatePicker>
+								</InputGroup>
+
+								<InputGroup>
+									<DatePicker
+										date={endDate}
+										setDate={(date) => setEndDate(date)}
+										open={openEndDate}
+										setOpen={() => setOpenEndDate((prev) => !prev)}
+										label="Date End"></DatePicker>
+								</InputGroup>
+							</div>
+
+							<InputGroup>
+								<InputGroupInput placeholder="photo cover Url" />
+								<InputGroupAddon>
+									<PhotoImageIcon size={4}></PhotoImageIcon>
 								</InputGroupAddon>
 								<InputGroupAddon align="inline-end">
 									<CheckIcon />
 									<InfoIcon />
 								</InputGroupAddon>
 							</InputGroup>
-
 							<InputGroup>
-								<InputGroupInput placeholder="Longitude" />
+								<InputGroupInput placeholder="Location" />
 								<InputGroupAddon>
-									<Map size={4} />
+									<Globe size={4} />
 								</InputGroupAddon>
 								<InputGroupAddon align="inline-end">
 									<CheckIcon />
 									<InfoIcon />
 								</InputGroupAddon>
 							</InputGroup>
-						</div>
+							<div className="grid grid-cols-2 gap-3">
+								<InputGroup>
+									<InputGroupInput placeholder="Latitude" />
+									<InputGroupAddon>
+										<Map size={4} />
+									</InputGroupAddon>
+									<InputGroupAddon align="inline-end">
+										<CheckIcon />
+										<InfoIcon />
+									</InputGroupAddon>
+								</InputGroup>
 
-						<div className="grid grid-cols-2 gap-4">
-							<InputGroup>
-								<InputGroupInput placeholder="Instagram" />
-								<InputGroupAddon>
-									<AtSymbol size={4}></AtSymbol>
-								</InputGroupAddon>
-								<InputGroupAddon align="inline-end">
-									<CheckIcon />
-									<InfoIcon />
-								</InputGroupAddon>
-							</InputGroup>
+								<InputGroup>
+									<InputGroupInput placeholder="Longitude" />
+									<InputGroupAddon>
+										<Map size={4} />
+									</InputGroupAddon>
+									<InputGroupAddon align="inline-end">
+										<CheckIcon />
+										<InfoIcon />
+									</InputGroupAddon>
+								</InputGroup>
+							</div>
 
-							<InputGroup>
-								<InputGroupInput placeholder="Komoot" />
-								<InputGroupAddon>
-									<AtSymbol size={4}></AtSymbol>
-								</InputGroupAddon>
-								<InputGroupAddon align="inline-end">
-									<CheckIcon />
-									<InfoIcon />
-								</InputGroupAddon>
-							</InputGroup>
+							<div className="grid grid-cols-2 gap-4">
+								<InputGroup>
+									<InputGroupInput placeholder="Instagram" />
+									<InputGroupAddon>
+										<AtSymbol size={4}></AtSymbol>
+									</InputGroupAddon>
+									<InputGroupAddon align="inline-end">
+										<CheckIcon />
+										<InfoIcon />
+									</InputGroupAddon>
+								</InputGroup>
 
-							<InputGroup>
-								<InputGroupInput placeholder="Strava" />
-								<InputGroupAddon>
-									<AtSymbol size={4}></AtSymbol>
-								</InputGroupAddon>
-								<InputGroupAddon align="inline-end">
-									<CheckIcon />
-									<InfoIcon />
-								</InputGroupAddon>
-							</InputGroup>
+								<InputGroup>
+									<InputGroupInput placeholder="Komoot" />
+									<InputGroupAddon>
+										<AtSymbol size={4}></AtSymbol>
+									</InputGroupAddon>
+									<InputGroupAddon align="inline-end">
+										<CheckIcon />
+										<InfoIcon />
+									</InputGroupAddon>
+								</InputGroup>
 
-							<InputGroup>
-								<InputGroupInput placeholder="Youtube" />
-								<InputGroupAddon>
-									<AtSymbol size={4}></AtSymbol>
-								</InputGroupAddon>
-								<InputGroupAddon align="inline-end">
-									<CheckIcon />
-									<InfoIcon />
-								</InputGroupAddon>
-							</InputGroup>
+								<InputGroup>
+									<InputGroupInput placeholder="Strava" />
+									<InputGroupAddon>
+										<AtSymbol size={4}></AtSymbol>
+									</InputGroupAddon>
+									<InputGroupAddon align="inline-end">
+										<CheckIcon />
+										<InfoIcon />
+									</InputGroupAddon>
+								</InputGroup>
 
-							<InputGroup>
-								<InputGroupInput placeholder="Other URL" />
-								<InputGroupAddon>
-									<AtSymbol size={4}></AtSymbol>
-								</InputGroupAddon>
-								<InputGroupAddon align="inline-end">
-									<CheckIcon />
-									<InfoIcon />
-								</InputGroupAddon>
-							</InputGroup>
+								<InputGroup>
+									<InputGroupInput placeholder="Youtube" />
+									<InputGroupAddon>
+										<AtSymbol size={4}></AtSymbol>
+									</InputGroupAddon>
+									<InputGroupAddon align="inline-end">
+										<CheckIcon />
+										<InfoIcon />
+									</InputGroupAddon>
+								</InputGroup>
+
+								<InputGroup>
+									<InputGroupInput placeholder="Other URL" />
+									<InputGroupAddon>
+										<AtSymbol size={4}></AtSymbol>
+									</InputGroupAddon>
+									<InputGroupAddon align="inline-end">
+										<CheckIcon />
+										<InfoIcon />
+									</InputGroupAddon>
+								</InputGroup>
+							</div>
+
+							<div></div>
 						</div>
 					</div>
 				</div>
+			</div>
+
+			<div>
+				<SectionTitle left={<></>} right={<>Preview</>}></SectionTitle>
 			</div>
 		</div>
 	);
---
+++ 2025/heyzenexblog-next/mock/storyContent.ts
@@ -0,0 +1,40 @@
+import { DataContentType } from "@/types/data";
+
+export const storyContentExample: DataContentType[] = [
+	{
+		sectionID: 1,
+		textContent:
+			"There is a reason why the sky is the limit. Because, wherever you are you will be just fine and you can do everything you think you can.",
+		photoUrls: [
+			{ url: "", description: "" },
+			{ url: "", description: "" },
+		],
+	},
+	{
+		sectionID: 2,
+		textContent:
+			"It started far before I have my own bike. I was with my cousin went somewhere with our bike. Our means family. Yes we had one bike for the whole family. That bike was old, broken in some parts, but still functioning ",
+		photoUrls: [
+			{ url: "", description: "" },
+			{ url: "", description: "" },
+		],
+	},
+	{
+		sectionID: 3,
+		textContent:
+			"This is Flamey. My first bike. That I ever bought with my own money. I rode Flamey for like 2 years. Well, he was like my baby at that time. I went with him here and there but I guess he was not really able to fulfil my expectation. Turns out I always ride through the gravel terrain. And I guess he suffered a lot. ",
+		photoUrls: [
+			{ url: "", description: "" },
+			{ url: "", description: "" },
+		],
+	},
+	{
+		sectionID: 3,
+		textContent:
+			"I decided to sell him. Oh God, I miss him. I meant, I really love that it can be flipped or folded and thrown to the back of the car or train. But yeah, it was  the old days and now I am happy with my new bike.",
+		photoUrls: [
+			{ url: "", description: "" },
+			{ url: "", description: "" },
+		],
+	},
+];
