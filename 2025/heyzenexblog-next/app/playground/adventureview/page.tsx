35130d5083ec1e3e8aab195678535c618a505c7b
Zen Huzaini <zen96ev@gmail.com>
2026-04-14 23:52:03 +0200

    add map

--- 2025/heyzenexblog-next/app/playground/adventureview/page.tsx
+++ 2025/heyzenexblog-next/app/playground/adventureview/page.tsx
@@ -1,12 +1,20 @@
 import { animationForArrow } from "@/animation/animation";
 import { PhotoHeaderSection } from "@/components/layout/StoryBookBlockLayouts/PhotoHeader";
 import { ImageWithSkeleton } from "@/components/molecule/ImageWithSkeleton";
+
 import ActivityAvatar from "@/components/molecule/User/Avatar/ActivityAvatar";
 
 import ChevronLeft from "@/icons/ChevronLeft";
 import ChevronRight from "@/icons/ChevronRight";
+import dynamic from "next/dynamic";
 
 import Link from "next/link";
+
+const BasicMap = dynamic(() => import("@/components/molecule/Map/BasicMap"), {
+	loading: () => (
+		<div className="h-[70vh] bg-gray-200 animate-pulse rounded-2xl" />
+	),
+});
 
 const page = () => {
 	return (
@@ -54,8 +62,50 @@ const page = () => {
 				</section>
 
 				{/* content */}
-				<section className="lg:ml-[20%] lg:mr-[20%] flex flex-col gap-2 sm:gap-5 mt-[3%] rounded-3xl dark:bg-transparent ">
+				<section className="flex flex-col gap-2 sm:gap-5 mt-[3%] rounded-3xl dark:bg-transparent ">
 					{/* <Content></Content> */}
+					{/* thi is supposed to be the contetn eith two columns. content in the left and map in the right */}
+
+					<div className="grid md:grid-cols-2">
+						{/* LEFT CONTENT */}
+						<div className="flex flex-col gap-5">
+							<h2 className="text-xl font-semibold">
+								Day 1 - the adventure begins
+							</h2>
+
+							<p>
+								Today was the day. After preparing for almost half year, I can’t
+								believe it was really happening...
+							</p>
+
+							<p>
+								The feeling of being far away from home and exploring new
+								places...
+							</p>
+
+							<p>I had a train to Zakopane from Wroclaw at 8:47...</p>
+
+							<p>After that I rode through the velo dunajec...</p>
+
+							<p>
+								After riding around 50km I arrived in the Frydman campsite...
+							</p>
+
+							<p>I started pitching my tent and then took a shower...</p>
+
+							{/* simulate long content */}
+							<div className="h-[120vh]" />
+						</div>
+
+						{/* RIGHT MAP (STICKY) */}
+						<div className="hidden lg:block">
+							<div className="sticky top-24">
+								<div className="w-full h-[70vh] rounded-2xl overflow-hidden">
+									<BasicMap></BasicMap>
+								</div>
+							</div>
+						</div>
+					</div>
 
 					<div className="flex justify-between mt-[5%]">
 						<Link href={`/`}>
