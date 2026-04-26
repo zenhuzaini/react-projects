b9cab2812f7d84a9fc3131f20f7464172d785c66
Zen Huzaini <zen96ev@gmail.com>
2026-04-26 21:23:23 +0200

    update page

--- 2025/heyzenexblog-next/components/molecule/User/Avatar/ActivityAvatar.tsx
+++ 2025/heyzenexblog-next/components/molecule/User/Avatar/ActivityAvatar.tsx
@@ -30,32 +30,27 @@ const ActivityAvatar = ({
 	const formattedDate = `${parts.weekday}, ${parts.month} ${parts.day} ${parts.year}`;
 
 	return (
-		<div className="flex flex-row items-center gap-1 md:gap-2">
-			<Avatar className="size-7 md:size-9">
+		<div className="mt-2 flex flex-row items-center gap-1 md:gap-2">
+			<Avatar className="size-9 md:size-9">
 				<AvatarImage src={url} alt={alt} />
 				<AvatarFallback>{avatarName}</AvatarFallback>
-				<AvatarBadge className="bg-green-600 dark:bg-green-800">
-					<CheckSymbol />
-				</AvatarBadge>
 			</Avatar>
 
-			<div>
-				<div className="flex flex-col text-xs md:text-sm">
-					<p className="text-primarytextInvert font-extralight">{avatarName}</p>
-					<p className="flex gap-1 text-[10px] items-center text-primarytextInvert md:text-sm tracking-tight">
-						went
-						<span className="flex gap-1 items-center">
-							<h6 className="text-primaryaccent font-extrabold flex">
-								<span className="text-dot">#</span>
-								{activityType}
-							</h6>
-
-							<div className="rounded-3xl h-4 w-0.5 bg-dot" />
-
-							{formattedDate}
+			<div className="flex flex-col text-sm md:text-lg">
+				<p className="text-primarytextInvert font-extralight">{avatarName}</p>
+				<p className="flex gap-1 text-[12px] items-center text-primarytextInvert md:text-sm tracking-tight">
+					went
+					<span className="flex gap-1 items-center">
+						<span className="text-primaryaccent font-extrabold flex">
+							<span className="text-dot">#</span>
+							{activityType}
 						</span>
-					</p>
-				</div>
+
+						<div className="rounded-3xl h-4 w-0.5 bg-dot" />
+
+						{formattedDate}
+					</span>
+				</p>
 			</div>
 		</div>
 	);
